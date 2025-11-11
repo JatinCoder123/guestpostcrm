60bd486f91d912cbff89c994045aa91a27d3cb10
Adityadav1149 <adityadav1119@gmail.com>
2025-11-11 14:34:22 +0530

    add endpoint for spam detection and click on any spam reason then show popup

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import ladgerReducer from "./Slices/ladger.js";
 import unrepliedReducer from "./Slices/unrepliedEmails.js";
 import unansweredReducer from "./Slices/unansweredEmails.js";
+import spamDetectionReducer from "./Slices/spamEmails.js";
 export const store = configureStore({
   reducer: {
     ladger: ladgerReducer,
     unreplied: unrepliedReducer,
     unanswered: unansweredReducer,
+    spamDetection: spamDetectionReducer,
   },
 });
