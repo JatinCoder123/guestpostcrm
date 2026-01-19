4abb9e7eb04d122a37f44a3b2acab4944723b5e3
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-01-19 16:15:06 +0530

    added the email stuff in recent activity

--- src/components/pages/RecentEntry.jsx
+++ src/components/pages/RecentEntry.jsx
@@ -1,8 +1,4 @@
-import {
-  CalendarDays,
-  Mail,
-  Activity,
-} from "lucide-react";
+import { CalendarDays, Mail, Activity } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect, useContext } from "react";
 import { getEvents } from "../../store/Slices/eventSlice";
@@ -16,7 +12,8 @@ export function RecentEntry() {
   const [showThread, setShowThread] = useState(false);
   const [currentThreadId, setCurrentThreadId] = useState(null);
   const [email, setEmail] = useState("");
-  const { setEnteredEmail, setWelcomeHeaderContent, setSearch } = useContext(PageContext);
+  const { setEnteredEmail, setWelcomeHeaderContent, setSearch } =
+    useContext(PageContext);
   const navigateTo = useNavigate();
 
   useEffect(() => {
@@ -35,19 +32,15 @@ export function RecentEntry() {
   }
   return (
     <div className="p-8">
-
       {/* Main Card */}
       <div className="bg-white shadow-lg rounded-xl overflow-hidden border border-gray-200">
-
         {/* Header */}
         <div className="bg-green-600 py-4 px-8">
           <div className="grid grid-cols-4 text-left text-white text-lg font-semibold">
             <div className="flex items-center gap-2">
               <CalendarDays size={22} /> DATE
             </div>
-            <div className="flex items-center gap-2">
-              CONTACT
-            </div>
+            <div className="flex items-center gap-2">CONTACT</div>
             <div className="flex items-center gap-2">
               <Mail size={22} /> EMAIL
             </div>
@@ -60,10 +53,7 @@ export function RecentEntry() {
 
         {/* Body */}
         <div className="px-8 py-4">
-
-          {loading && (
-            <p className="text-center py-4 text-lg">Loading...</p>
-          )}
+          {loading && <p className="text-center py-4 text-lg">Loading...</p>}
 
           {!loading && events.length === 0 && (
             <p className="text-center text-lg py-4">No events found</p>
@@ -76,19 +66,23 @@ export function RecentEntry() {
               className="grid grid-cols-4 py-5 border-b text-gray-800 hover:bg-gray-50 transition"
             >
               {/* Date */}
-              <div className="flex items-center gap-3 text-[17px]" onClick={() => {
-                const input = extractEmail(event.name);
-                localStorage.setItem("email", input);
-                setSearch(input);
-                setEnteredEmail(input);
-                setWelcomeHeaderContent("Unreplied");
-                navigateTo("/");
-              }}>
+              <div
+                className="flex items-center gap-3 text-[17px]"
+                onClick={() => {
+                  const input = extractEmail(event.name);
+                  localStorage.setItem("email", input);
+                  setSearch(input);
+                  setEnteredEmail(input);
+                  setWelcomeHeaderContent("Unreplied");
+                  navigateTo("/");
+                }}
+              >
                 <CalendarDays size={20} className="text-green-700" />
                 {event.date_entered ?? "—"}
               </div>
 
-              <div className="flex items-center gap-3 text-[17px] text-blue-600  cursor-pointer"
+              <div
+                className="flex items-center gap-3 text-[17px] text-blue-600  cursor-pointer"
                 onClick={() => {
                   const input = excludeName(event.real_name);
                   localStorage.setItem("email", input);
@@ -96,13 +90,13 @@ export function RecentEntry() {
                   setEnteredEmail(input);
                   setWelcomeHeaderContent("Unreplied");
                   navigateTo("/contacts");
-                }}>
-
-
+                }}
+              >
                 {excludeName(event.real_name) ?? "—"}
               </div>
 
-              <div className="flex items-center gap-3 text-[17px] text-blue-600 underline cursor-pointer"
+              <div
+                className="flex items-center gap-3 text-[17px] text-blue-600 underline cursor-pointer"
                 onClick={() => {
                   const input = excludeEmail(event.real_name);
                   localStorage.setItem("email", input);
@@ -115,7 +109,9 @@ export function RecentEntry() {
                 }}
               >
                 <Mail size={20} className="text-blue-600" />
-                {excludeEmail(event.real_name) ?? "—"}
+                {excludeEmail(
+                  event.real_name == `User` ? event.name : event.real_name
+                ) ?? "—"}
               </div>
 
               {/* Recent Activity */}
