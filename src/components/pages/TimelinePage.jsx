b6102a1031b5ab2a429c4d16f5ada0879f33fd1b
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-01-29 13:26:54 +0530

    latest merge by kamal

--- src/components/pages/TimelinePage.jsx
+++ src/components/pages/TimelinePage.jsx
@@ -219,6 +219,7 @@ export function TimelinePage() {
         buttonLabel="Send"
         loading={aiReplySentLoading}
         onUpdate={(data) => {
+          console.log(data);
           setAiReply(data.ai_reply);
           handleAiAutoReply(data.ai_reply);
           setShowUpdateAiReply(false);
@@ -366,10 +367,11 @@ export function TimelinePage() {
                       {viewEmail?.length > 0 && (
                         <div
                           className={`inline-flex items-center gap-2 px-3 py-1.5 rounded-full text-xs font-semibold
-      ${viewEmail[viewEmail.length - 1].from_email === email
-                              ? "bg-green-100 text-green-700"
-                              : "bg-blue-100 text-blue-700"
-                            }
+      ${
+        viewEmail[viewEmail.length - 1].from_email === email
+          ? "bg-green-100 text-green-700"
+          : "bg-blue-100 text-blue-700"
+      }
     `}
                         >
                           <Mail className="w-4 h-4" />
@@ -420,14 +422,14 @@ export function TimelinePage() {
               {!(
                 !mailersSummary || Object.keys(mailersSummary).length === 0
               ) && (
-                  <ActionButton
-                    handleMoveSuccess={handleMoveSuccess}
-                    setShowEmails={setShowEmail}
-                    setShowIP={setShowIP}
-                    threadId={currentThreadId}
-                    handleActionBtnClick={handleActionBtnClick}
-                  />
-                )}
+                <ActionButton
+                  handleMoveSuccess={handleMoveSuccess}
+                  setShowEmails={setShowEmail}
+                  setShowIP={setShowIP}
+                  threadId={currentThreadId}
+                  handleActionBtnClick={handleActionBtnClick}
+                />
+              )}
             </div>
 
             {ladger?.length > 0 ? (
