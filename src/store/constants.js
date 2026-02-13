51383fcf2f8ed8d14ec6c4b41a947a1daacc59c8
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-02-13 15:57:05 +0530

    added the tinny stuff

--- src/store/constants.js
+++ src/store/constants.js
@@ -3,5 +3,5 @@ export const CREATE_DEAL_URL =
   "https:/crm.outrightsystems.org/index.php?entryPoint=get_post_all";
 export const CREATE_DEAL_API_KEY = "FldBjAIfoBo2UTcBAezvTOQg9";
 export const TINY_EDITOR_API_KEY =
-  "2kf8gyd7ms1x661uenztdjekg8v7b9iw4eageav7rlqiekjg";
+  "2f8096h5xt9z0oi2cmdjt3klvuz43rax7t42mwi0qqbdjedf";
 export const LOCAL_KEY = "create_deals_draft_v1";
