516106b148e87559ffe0dfb8fd1b758870947bca
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-02-17 16:11:37 +0530

    Fixing up down btn

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -534,6 +534,30 @@ const TimelineEvent = () => {
         >
           TIMELINE
         </h1>
+        <div class="flex justify-center mt-10">
+          <div class="relative w-80">
+            <input
+              type="text"
+              placeholder="Search..."
+              class="w-full pl-10 pr-4 py-2 rounded-full border border-gray-300 
+             focus:outline-none focus:ring-2 focus:ring-blue-500 
+             focus:border-transparent shadow-sm transition duration-300"
+            />
+            <svg
+              class="absolute left-3 top-2.5 w-5 h-5 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-4.35-4.35m1.85-5.65a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+        </div>
 
         <div className="flex justify-center mt-6">
           <div className="relative flex bg-gray-100 rounded-full p-1 w-[360px] shadow-inner">
