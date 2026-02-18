ef14f3dfd2c8d508e860a521e2feaaaf16126362
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-02-18 10:52:52 +0530

    unwanted stuff

--- src/components/pages/TimelinePage.jsx
+++ src/components/pages/TimelinePage.jsx
@@ -21,7 +21,6 @@ import { NoSearchFoundPage } from "../NoSearchFoundPage";
 import { SocketContext } from "../../context/SocketContext";
 import { useNavigate } from "react-router-dom";
 import { PreviewTemplate } from "../PreviewTemplate";
-import PromptViewerModal from "../PromptViewerModal";
 
 const decodeHTMLEntities = (str = "") => {
   if (typeof str !== "string") return str;
@@ -41,8 +40,6 @@ export function TimelinePage() {
   const [showAvatar, setShowAvatar] = useState(false);
   const [editorContent, setEditorContent] = useState("");
   const [showPreview, setShowPreview] = useState(false);
-  const [open, setOpen] = useState(false)
-  const [selectedPrompt, setSelectedPrompt] = useState(null)
   const {
     error: sendError,
     message,
@@ -93,7 +90,9 @@ export function TimelinePage() {
       toast.info("No Unreplied Email found");
       return;
     }
-    dispatch(sendEmail({ reply: btnBody, threadId: "Quick Action Button Reply Sent" }));
+    dispatch(
+      sendEmail({ reply: btnBody, threadId: "Quick Action Button Reply Sent" }),
+    );
     dispatch(
       addEvent({
         email: email,
@@ -103,8 +102,20 @@ export function TimelinePage() {
     );
   };
 
+  const handleMoveToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   const handleAiAutoReply = () => {
-    dispatch(sendEmail({ reply: editorContent, threadId: "Ai Reply Send Successfully" }));
+    dispatch(
+      sendEmail({
+        reply: editorContent,
+        threadId: "Ai Reply Send Successfully",
+      }),
+    );
   };
   const handleNext = () => {
     if (currentIndex < emails?.length - 1) {
@@ -165,233 +176,15 @@ export function TimelinePage() {
 
   return (
     <>
-      {open && (
-        <PromptViewerModal
-          promptDetails={selectedPrompt}
-          onClose={() => setOpen(false)}
-        />
-      )}
-      <div className="bg-white rounded-2xl shadow-sm min-h-[400px]">
-        {(loading || unrepliedLoading) && <LoadingSkeleton />}
-        {!loading && !unrepliedLoading && (
-          <>
-            <div className="flex flex-col p-6 border-b border-gray-200">
-              <ContactHeader
-                onNext={handleNext}
-                onPrev={handlePrev}
-                currentIndex={currentIndex}
-              />
-
-              <MailerSummaryHeader />
-
-              <div className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-4">
-                {/* AI SUMMARY */}
-                <div className="bg-green-50 border border-green-200 rounded-lg p-4 h-56 overflow-y-auto">
-                  <div className="flex items-center justify-start gap-2 mb-2">
-                    <h3 className="text-green-700 font-semibold">
-                      Quick Reply
-                    </h3>
-                    {/* Send AI Reply Button - Moved to top right */}
-
-                    <motion.button
-                      whileHover={{ scale: 1.15 }}
-                      whileTap={{ scale: 0.95 }}
-                      transition={{ type: "spring", stiffness: 400 }}
-                      className="flex items-center justify-center gap-2 bg-green-500 hover:bg-green-600 text-white py-2 px-2 rounded-full transition-colors disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
-                      onClick={() => setShowPreview(true)}
-                      disabled={
-                        sending ||
-                        mailersSummary == null ||
-                        mailersSummary?.ai_response?.trim() === ""
-                      }
-                    >
-                      <img
-                        width="33"
-                        height="33"
-                        src="https://img.icons8.com/ultraviolet/40/bot.png"
-                        alt="AI Reply"
-                      />
-                    </motion.button>
-                    <div className="flex items-center justify-center ml-4 ">
-                      {mailersSummary?.prompt_details
-                        && (
-                          <button
-                            onClick={() => {
-                              setSelectedPrompt(mailersSummary?.prompt_details
-                              );
-                              setOpen(true);
-                            }
-                            }
-                            className="text-green-600 hover:text-green-700 cursor-pointer"
-                          >
-                            <SparkleIcon size={20} />
-                          </button>
-                        )}
-                    </div>
-                  </div>
-
-                  {!sending && (
-                    <div className="mb-3">
-                      <p className="text-gray-700 text-sm leading-relaxed whitespace-pre-line">
-                        {aiReply}
-                      </p>
-                    </div>
-                  )}
-                </div>
-
-                <div className="bg-blue-50 border border-blue-200 rounded-lg p-4 h-56 overflow-y-auto">
-                  <div className="flex items-center mb-2">
-                    <h3 className="text-blue-700 font-semibold">AI Summary</h3>
-
-                    <motion.button
-                      whileHover={{ scale: 1.15 }}
-                      whileTap={{ scale: 0.95 }}
-                      transition={{ type: "spring", stiffness: 400 }}
-                      className="rounded-full bg-white/90 shadow-lg hover:shadow-xl border border-gray-200 p-1 ml-2 cursor-pointer"
-                      onClick={() => {
-                        dispatch(getAvatar());
-                        setShowAvatar(true);
-                      }}
-                    >
-                      <img
-                        width="40"
-                        height="40"
-                        src="https://img.icons8.com/office/40/circled-play.png"
-                        alt="Play AI Avatar"
-                      />
-                    </motion.button>
-                    {/* PROMPT */}
-                    <div className="flex items-center justify-center ml-4">
-                      {mailersSummary?.prompt_details
-                        && (
-                          <button
-                            onClick={() => {
-                              setSelectedPrompt(mailersSummary?.prompt_details
-                              );
-                              setOpen(true);
-                            }
-                            }
-                            className="text-blue-600 hover:text-blue-700 cursor-pointer"
-                          >
-                            <SparkleIcon size={20} />
-                          </button>
-                        )}
-                    </div>
-                  </div>
-
-                  <p className="text-gray-700 text-sm leading-relaxed">
-                    {decodeHTMLEntities(
-                      mailersSummary?.summary ?? "No AI summary available.",
-                    )}
-                  </p>
-                </div>
-
-                {/* Latest Message */}
-
-                <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4 h-56 overflow-y-auto custom-scrollbar shadow-sm">
-                  <div className="flex flex-col justify-center mb-2">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <h3 className="text-yellow-700 font-semibold">
-                          Latest Message
-                        </h3>
-                        <motion.button
-                          whileHover={{ scale: 1.15 }}
-                          whileTap={{ scale: 0.95 }}
-                          transition={{ type: "spring", stiffness: 400 }}
-                          className="flex items-center gap-2 rounded-full bg-white/90 shadow-lg hover:shadow-xl border border-gray-200 p-2 ml-2 cursor-pointer"
-                          onClick={() => {
-                            if (emails.length > 0) {
-                              setShowThread(true);
-                            } else {
-                              setShowEmail(true);
-                            }
-                          }}
-                        >
-                          <Reply className="w-6 h-6 text-yellow-700" />
-                        </motion.button>
-                      </div>
-
-                      {viewEmail?.length > 0 && (
-                        <div
-                          className={`inline-flex items-center gap-2 px-3 py-1.5 rounded-full text-xs font-semibold
-      ${viewEmail[viewEmail.length - 1].from_email === email
-                              ? "bg-green-100 text-green-700"
-                              : "bg-blue-100 text-blue-700"
-                            }
-    `}
-                        >
-                          <Mail className="w-4 h-4" />
-
-                          {viewEmail[viewEmail.length - 1].from_email === email
-                            ? "Client Mail"
-                            : "Our Mail"}
-                        </div>
-                      )}
-                    </div>
-
-                    <div className="flex items-center gap-2">
-                      {viewEmail?.length > 0 && (
-                        <>
-                          <span className="text-xs text-gray-500">
-                            {viewEmail[viewEmail.length - 1]?.date_created}{" "}
-                            <br />
-                          </span>
-                          <span className="text-xs text-gray-500">
-                            ({" "}
-                            {viewEmail[viewEmail.length - 1]?.date_created_ago}{" "}
-                            )<br />
-                          </span>
-                        </>
-                      )}
-                    </div>
-                  </div>
-                  {viewEmailLoading ? (
-                    <LoadingSkeleton
-                      className="h-56"
-                      count={1}
-                      width="100%"
-                      height="100%"
-                    />
-                  ) : (
-                    <div
-                      className="text-gray-700 text-sm leading-relaxed whitespace-pre-line"
-                      dangerouslySetInnerHTML={{
-                        __html:
-                          viewEmail?.length > 0
-                            ? viewEmail[viewEmail.length - 1]?.body
-                            : "No Message Found!",
-                      }}
-                    />
-                  )}
-                </div>
-              </div>
-              {!(
-                !mailersSummary || Object.keys(mailersSummary).length === 0
-              ) && (
-                  <ActionButton
-                    handleMoveSuccess={handleMoveSuccess}
-                    setShowEmails={setShowEmail}
-                    setShowIP={setShowIP}
-                    handleActionBtnClick={handleActionBtnClick}
-                  />
-                )}
-            </div>
-
-            {ladger?.length > 0 ? (
-              <TimelineEvent />
-            ) : (
-              <div className="py-[2%] px-[30%] ">
-                <h1 className="font-mono text-2xl bg-gradient-to-r from-purple-600 to-blue-600 p-2 rounded-2xl text-center text-white">
-                  TIMELINE
-                </h1>
-                <p className="text-gray-700 text-sm text-center leading-relaxed mt-2">
-                  No timeline events found.
-                </p>
-              </div>
-            )}
-          </>
-        )}
+      <div className="fixed bottom-6 right-6 z-50">
+        <button
+          onClick={handleMoveToTop}
+          className="bg-gradient-to-r from-purple-600 to-blue-600 
+               text-white px-4 py-2 rounded-full shadow-lg
+               hover:scale-110 transition-all duration-300"
+        >
+          Move To Top ↑
+        </button>
       </div>
 
       {showAvatar && <Avatar setShowAvatar={setShowAvatar} onPlay={true} />}
