bd57f68ed9fafeb2507b80ae92f13527a82d16b8
Jatin Verma <verm.jatin2004@gmail.com>
2026-02-20 10:29:55 +0530

    Add Refresh ladger funtion in useIdle

--- src/hooks/useIdle.js
+++ src/hooks/useIdle.js
@@ -1,10 +1,50 @@
 import React, { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../context/SocketContext";
 import useRefresh from "./useRefresh";
+import { useDispatch, useSelector } from "react-redux";
+import { PageContext } from "../context/pageContext";
+import { getLadger } from "../store/Slices/ladger";
+import { getUnrepliedEmail } from "../store/Slices/unrepliedEmails";
+import { getUnansweredEmails } from "../store/Slices/unansweredEmails";
+import { viewEmailAction } from "../store/Slices/viewEmail";
+import { getOrders } from "../store/Slices/orders";
+import { getInvoices } from "../store/Slices/invoices";
+import { getOffers } from "../store/Slices/offers";
+import { getDeals } from "../store/Slices/deals";
 
 
 function useIdle({ idle }) {
     const { setUserIdle, eventQueueRef, setEventQueue, } = useContext(SocketContext);
+    const {
+        enteredEmail,
+        currentIndex,
+        setCurrentIndex,
+        search,
+        setWelcomeHeaderContent,
+    } = useContext(PageContext);
+    const dispatch = useDispatch();
+    const { emails, loading } = useSelector((state) => state.unreplied);
+    const [firstEmail, setFirstEmail] = useState(null);
+
+    const refreshLadger = () => {
+        if (enteredEmail) {
+            dispatch(getLadger({ email: enteredEmail, search }));
+        } else if (firstEmail) {
+            dispatch(getLadger({ email: firstEmail, search }));
+        } else if (!loading) {
+            dispatch(getLadger({ search, isEmail: false }));
+        }
+        dispatch(getUnrepliedEmail({ email: enteredEmail, loading: false }));
+        dispatch(getUnansweredEmails({ email: enteredEmail, loading: false }));
+        dispatch(viewEmailAction.resetViewEmail());
+    };
+    useEffect(() => {
+        if (emails?.length > 0) {
+            setFirstEmail(
+                emails[currentIndex].from?.match(/[\w.-]+@[\w.-]+\.\w+/)?.[0],
+            );
+        }
+    }, [emails?.length, currentIndex]);
     const flushQueue = () => {
         const queue = eventQueueRef.current; // ✅ THIS IS THE REAL QUEUE
         console.log("FLUSHING QUEUE", queue);
