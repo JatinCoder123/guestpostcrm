4a004e3350a63808e3f655d7531647c0a4de68ab
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-03-27 13:40:42 +0530

    added the visulaization check

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -427,7 +427,7 @@ const TimelineEvent = ({ handleMessageClick }) => {
                           </button>
                         )}
                         {/* Visualization */}
-                        {event.visualization && (
+                        {event.visualization.length > 0 && (
                           <button
                             onClick={() => {
                               setActiveVisualization(event.visualization);
