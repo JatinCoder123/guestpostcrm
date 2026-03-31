b0e4b19a16e4cbadac3f2282d07973527c0624b9
Jatin Verma <verm.jatin2004@gmail.com>
2026-03-31 17:58:07 +0530

    Change postion for table footer

--- src/components/ui/table/TableTitleBar.jsx
+++ src/components/ui/table/TableTitleBar.jsx
@@ -1,5 +1,6 @@
 
 import { BadgeInfo, Download } from "lucide-react"
+import TableFooter from "./TableFooter"
 
 
 function TableTitleBar({ Icon, title, iconClass, titleClass, count }) {
@@ -7,13 +8,14 @@ function TableTitleBar({ Icon, title, iconClass, titleClass, count }) {
         <div className="flex items-center font-bold text-xl justify-between p-4 border-b bg-gray-50">
 
             {/* Left */}
-            <div className={`flex items-center gap-2 text-gray-700 font-semibold ${titleClass}`}>
+            <div className={`flex shirnk-0 items-center gap-2 text-gray-700 font-semibold ${titleClass}`}>
                 <Icon className={`w-6 h-6  ${iconClass}`} />
                 {title}
                 <button on><BadgeInfo /></button>
+
             </div>
 
-
+            <TableFooter />
 
 
         </div>
