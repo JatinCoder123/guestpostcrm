4bce245466e9a361b38848f3ae3f5c83725f6bd7
Jatin Verma <verm.jatin2004@gmail.com>
2026-04-20 11:08:52 +0530

    Add cache in threadEmail

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,36 +1,38 @@
 const viewMails = {};
+const threadMails = {};
 const ledgers = {};
 const contacts = {};
 
 const cacheStore = {
     viewMails,
+    threadMails,
     ledgers,
     contacts,
 };
 
 
-export const setCache = (section, email, data) => {
+export const setCache = (section, key, data) => {
     if (!cacheStore[section]) {
         console.warn(`Invalid cache section: ${section}`);
         return;
     }
 
-    cacheStore[section][email] = data;
+    cacheStore[section][key] = data;
 };
 
-export const getCache = (section, email) => {
+export const getCache = (section, key) => {
     if (!cacheStore[section]) {
         console.warn(`Invalid cache section: ${section}`);
         return null;
     }
 
-    return cacheStore[section][email] || null;
+    return cacheStore[section][key] || null;
 };
 
 
-export const clearCache = (section, email) => {
-    if (cacheStore[section]?.[email]) {
-        delete cacheStore[section][email];
+export const clearCache = (section, key) => {
+    if (cacheStore[section]?.[key]) {
+        delete cacheStore[section][key];
     }
 };
 
