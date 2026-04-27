f8cda46ce146ffc3470621ef7c323d97643e1a0e
Jatin Verma <verm.jatin2004@gmail.com>
2026-04-27 16:28:45 +0530

    Fix market place and remove to

--- src/components/pages/TimelinePage.jsx
+++ src/components/pages/TimelinePage.jsx
@@ -12,20 +12,11 @@ import { NoSearchFoundPage } from "../NoSearchFoundPage";
 import MessageModal from "../MessageModal";
 import LatestMessage from "../LatestMessage";
 export function TimelinePage() {
-  const [showIP, setShowIP] = useState(false);
   const [showAvatar, setShowAvatar] = useState(false);
   const [selectedMessage, setSelectedMessage] = useState(null);
   const [showMessageModal, setShowMessageModal] = useState(false);
-  const [isMark, setIsMark] = useState(false);
-  const { items: marketPlaces } = useSelector((s) => s.marketplace);
 
-  useEffect(() => {
-    if (marketPlaces.length > 0) {
-      setIsMark(marketPlaces.find((e) => e.name === contactInfo?.email1));
-    } else {
-      setIsMark(false);
-    }
-  }, [marketPlaces]);
+
   const { loading: ladgerLoading, ladger } = useSelector((state) => state.ladger);
 
   const handleMessageClick = (id) => {
@@ -57,13 +48,13 @@ export function TimelinePage() {
       <div className="bg-white rounded-2xl shadow-sm min-h-[400px]">
         {(unrepliedLoading || contactLoading || loading || ladgerLoading) ? <LoadingSkeleton /> : <>
           <div className="flex flex-col  border-b border-gray-200">
-            <ContactHeader isMark={isMark} />
+            <ContactHeader />
 
             <div className="mt-2 p-2 grid grid-cols-1 md:grid-cols-2 gap-4 ">
               <MailerSummaryHeader />
               <LatestMessage handleMessageClick={handleMessageClick} />
             </div>
-            <ActionButton isMark={isMark} />
+            <ActionButton />
           </div>
           <TimelineEvent handleMessageClick={handleMessageClick} />
 
