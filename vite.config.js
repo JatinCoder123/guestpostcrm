7d82f4f763b21a5141b5b160abd2f1cd0b6731e6
kamalUniyal06 <kamaluniyal19@gmail.com>
2026-05-04 12:01:36 +0530

    added the master code

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,33 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://app.guestpostcrm.com/",
+  base: "https://appstage.guestpostcrm.com/",
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://errika.guestpostcrm.com',
+      "/api": {
+        target: "https://errika.guestpostcrm.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
       },
-      '/invoice-api': {
-        target: 'https://errika.guestpostcrm.com',
+      "/invoice-api": {
+        target: "https://errika.guestpostcrm.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/invoice-api/, ''),
-        secure: false
-      }
-    }
+        rewrite: (path) => path.replace(/^\/invoice-api/, ""),
+        secure: false,
+      },
+    },
   },
   build: {
     // Build optimization
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          redux: ['@reduxjs/toolkit', 'react-redux']
-        }
-      }
-    }
-  }
+          vendor: ["react", "react-dom"],
+          redux: ["@reduxjs/toolkit", "react-redux"],
+        },
+      },
+    },
+  },
 });
