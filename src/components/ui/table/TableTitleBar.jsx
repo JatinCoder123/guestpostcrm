aa3578031b7985bec5731a2c6299f35f0061c80c
Jatin Verma <verm.jatin2004@gmail.com>
2026-06-23 15:55:17 +0530

    Fix back button in all table

--- src/components/ui/table/TableTitleBar.jsx
+++ src/components/ui/table/TableTitleBar.jsx
@@ -1,23 +1,28 @@
-import { BadgeInfo } from "lucide-react";
+import { ArrowLeft, BadgeInfo } from "lucide-react";
 import TableFooter from "./TableFooter";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import IconButton from "../Buttons/IconButton";
 function TableTitleBar({ Icon, title, iconClass, titleClass }) {
     const navigate = useNavigate();
 
     return (
         <div className="flex items-center justify-between px-5 py-4 border-b bg-white shadow-sm rounded-t-xl">
-            <button
-  onClick={() => navigate("/view-reports")}
-  className="px-5 py-2.5 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg transition-all duration-200"
->
-  Back
-</button>
+
             <div className={`flex items-center gap-3 text-gray-800 ${titleClass}`}>
-                <div className="p-2 rounded-lg ">
+                <IconButton
+                    onClick={() => navigate(-1)}
+                    className={`h-10 w-10 rounded-full border bg-white hover:bg-gray-100 transition flex items-center justify-center ${titleClass}`}
+                    icon={ArrowLeft}
+                    label="Back"
+                    tooltipPosition="bottom"
+                />
+                <div className="p-2 rounded-lg flex items-center gap-3">
+
                     <Icon className={`w-5 h-5 ${iconClass}`} />
+                    <h2 className="text-lg font-semibold">{title}</h2>
                 </div>
 
-                <h2 className="text-lg font-semibold">{title}</h2>
+
 
                 <button className="p-1 hover:bg-gray-100 rounded-md transition">
                     <BadgeInfo className="w-4 h-4 text-gray-500" />
