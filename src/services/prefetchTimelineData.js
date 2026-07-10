54ac753634a92e7509984cff68cc4a62338735da
Jatin Verma <verm.jatin2004@gmail.com>
2026-07-10 09:45:05 +0530

    Prefetching invoice

--- src/services/prefetchTimelineData.js
+++ src/services/prefetchTimelineData.js
@@ -55,7 +55,6 @@ export const prefetchTimelineData = async (
             queryFn: () =>
                 getDealsByEmail(email),
         }),
-
         queryClient.prefetchQuery({
             queryKey:
                 offerKeys.byEmail(email),
